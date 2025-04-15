c762aae5e236c7adb9f0dab2e60c03e419a417df
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2025-04-15 16:08:32 +0500

    egui 0.31

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,4 @@
-//! # egui_clickpack_db
-//!
-//! ClickpackDB integration for `egui`.
-//!
-//! ![ClickpackDB](https://github.com/zeozeozeo/zcb3/raw/master/screenshots/3.png?raw=true)
-//!
-//! ![Good luck GIF](https://github.com/me-shaon/GLWTPL/blob/master/good-luck.gif?raw=true)
+#![doc = include_str!("../README.md")]
 
 use egui::Color32;
 use egui_extras::{Column, TableBuilder};
